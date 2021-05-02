264a66048fc09d2a2062ac033c5877e19a7f10a3
Kosolapov Nikolai <69964955+55Kosolapov5Nikolai55@users.noreply.github.com>
2021-05-02 15:21:01 +0300

    Printing of Triangle

---
+++ Printing of Triangle/src/Kosolapov/Nikolai/Main.java
@@ -0,0 +1,20 @@
+package Kosolapov.Nikolai;
+
+public class Main {
+
+    // Triangle printing:
+
+    public static void printTriangle(int n , String element){
+        for(int printIndex = 0; printIndex < n; printIndex ++){
+            for(int displayIndex = 0; displayIndex < printIndex + 1; displayIndex ++) {
+                System.out.print(element);
+            }
+            System.out.println("\n");
+        }
+    }
+
+    public static void main(String[] args) {
+        printTriangle(10 , "{}");
+    }
+}
+   
