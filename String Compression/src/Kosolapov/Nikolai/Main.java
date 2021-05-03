c1e4062b53220ee06908f8f86cb2dd5c7d2fb07e
Kosolapov Nikolai <69964955+55Kosolapov5Nikolai55@users.noreply.github.com>
2021-05-03 23:36:36 +0300

    String Compression

--- String Compression/src/Kosolapov/Nikolai/Main.java
+++ String Compression/src/Kosolapov/Nikolai/Main.java
@@ -11,6 +11,7 @@ public class Main {
             System.out.print(quantities.get(displayResults));
             System.out.print(componentsResults.get(displayResults) + " ; ");
         }
+        System.out.println();
     }
 
     public static void compressString(String stringToCompress) {
@@ -19,7 +20,7 @@ public class Main {
         LinkedList<Character> history = new LinkedList<>();
         LinkedList<Character> componentsResults = new LinkedList<>();
         LinkedList<Integer> quantities = new LinkedList<>();
-        for(int currentIndex = 0; currentIndex < components.length - 1; currentIndex ++){
+        for(int currentIndex = 0; currentIndex < components.length; currentIndex ++){
             if(!history.contains(components[currentIndex])) {
                 history.add(components[currentIndex]);
                 for (int compareIndex = currentIndex + 1; compareIndex < components.length; compareIndex++) {
@@ -40,7 +41,9 @@ public class Main {
     public static void main(String[] args) {
 
         compressString("asdfbbcaaa");
-
+        compressString("^   "); // " " as a component
+        compressString("ggggh");
+        compressString("hgggg");
     }
 }
    
