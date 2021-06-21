67561770494d67075bf58542efe5505e650f636a
Kosolapov Nikolai <69964955+KosolapovNikolai05@users.noreply.github.com>
2021-06-21 21:38:01 +0300

    Turing's Machine Realization 2nd part.

--- Turing's Machine/src/Kosolapov/Nikolai/Main.java
+++ Turing's Machine/src/Kosolapov/Nikolai/Main.java
@@ -5,20 +5,36 @@ import java.util.LinkedList;
 public class Main {
 
     public static void main(String[] args) {
+
+        System.out.println("First example:");
         LinkedList<String> alphabet = new LinkedList<>();
         alphabet.add("X");
         alphabet.add("Y");
         alphabet.add("Z");
-        String [] tape = {null , "X" , "Y" , "Z"};
-        TuringMachine floatingPointMachine = new TuringMachine(tape , alphabet , 0);
-        floatingPointMachine.writeCommand("a0" , "R");
-        floatingPointMachine.writeCommand("Z" , "R");
-        floatingPointMachine.writeCommand("Y" , "R");
-        floatingPointMachine.writeCommand("X" , "L");
-        floatingPointMachine.writeCommand("Y" , "L");
-        floatingPointMachine.writeCommand("Z" , "N");
-        floatingPointMachine.displayTape();
-        System.out.print("Current position: ");
-        floatingPointMachine.displayCurrentPosition();
+        String [] tape = {null , null ,  "X" , "Y" , "Z"};
+        LinkedList<TuringMachine.Condition> conditionsMT = new LinkedList<>();
+        TuringMachine.Condition q0 = new TuringMachine.Condition();
+        TuringMachine.Condition q2ForZ = new TuringMachine.Condition("X" , "B" , "L" , q0);
+        TuringMachine.Condition q1ForZ = new TuringMachine.Condition("Z" , "X" , "N" , q2ForZ);
+        TuringMachine.Condition q1ForY = new TuringMachine.Condition("Y" , "Y" , "R" , q1ForZ);
+        TuringMachine.Condition q1ForX = new TuringMachine.Condition("X" , "Z" , "R" , q1ForY);
+        TuringMachine.Condition q1ForA0 = new TuringMachine.Condition("a0" , "a0" , "R" , q1ForX);
+        TuringMachine MT = new TuringMachine(tape , alphabet , conditionsMT , 0 , q1ForA0 , q0);
+        MT.runMachine();
+        MT.displayCurrentIndex();
+        System.out.println("Second example:");
+        LinkedList<String> alphabet1 = new LinkedList<>();
+        alphabet1.add("1");
+        alphabet1.add("+");
+        String [] tape1 = {"1" , "1" , "1" , "1" , "1" , "+" , "1" , "1" , "1" , "1" , "1" , null};
+        LinkedList<TuringMachine.Condition> conditionsMT1 = new LinkedList<>();
+        TuringMachine.Condition q3For1 = new TuringMachine.Condition("1" , "a0" , "L" , q0);
+        TuringMachine.Condition q2ForA0 = new TuringMachine.Condition("a0" , "a0" , "L" , q3For1);
+        TuringMachine.Condition q2For1 = new TuringMachine.Condition("1" , "1" , "R" , q2ForA0);
+        TuringMachine.Condition q1ForPlus = new TuringMachine.Condition("+" , "1" , "R" , q2For1);
+        TuringMachine.Condition q1For1 = new TuringMachine.Condition("1" , "1" , "R" , q1ForPlus);
+        TuringMachine MT1 = new TuringMachine(tape1 , alphabet1 , conditionsMT1 , 0 , q1For1 , q0);
+        MT1.runMachine();
+        MT1.displayCurrentIndex();
     }
 }
--- Turing's Machine/src/Kosolapov/Nikolai/TuringMachine.java
+++ Turing's Machine/src/Kosolapov/Nikolai/TuringMachine.java
@@ -1,78 +1,130 @@
 package Kosolapov.Nikolai;
 
 import java.util.LinkedList;
+import java.util.Objects;
 
 public class TuringMachine {
 
-    private String [] tape;
+    private String[] tape;
     private LinkedList<String> alphabet;
+    private static LinkedList<Condition> conditionsMT;
+    private Condition currentCondition;
+    private Condition stopCondition;
     private int currentIndex;
 
-
-
-    public void displayTape () {
+    private void displayTape() {
         for (String displayValue : tape) {
-            System.out.println(displayValue + " ");
+            System.out.print(displayValue + " ");
         }
+        System.out.println("|");
     }
 
-    public void displayCurrentPosition () {
-        System.out.println(currentIndex);
-    }
-
-    public boolean correctTape (String[] tape) {
+    private boolean isCorrectTape(String[] tape) {
         int inAlphabetCheckCounter = 0;
-        for (int inAlphabetCheckIndex = 0; inAlphabetCheckIndex < tape.length; inAlphabetCheckIndex ++) {
-            if(alphabet.contains(tape[inAlphabetCheckIndex])){
-                ++ inAlphabetCheckCounter;
+        for (int inAlphabetCheckIndex = 0; inAlphabetCheckIndex < tape.length; inAlphabetCheckIndex++) {
+            if (alphabet.contains(tape[inAlphabetCheckIndex])) {
+                ++inAlphabetCheckCounter;
             }
         }
         return inAlphabetCheckCounter == tape.length;
     }
 
-    private void fillUpTape (String[] tape) {
+    private void fillUpTape(String[] tape) {
 
-        if(tape.length > 0) {
+        if (tape.length > 0) {
             for (int fillUpIndex = 0; fillUpIndex < tape.length; fillUpIndex++) {
                 if (tape[fillUpIndex] == null) {
                     tape[fillUpIndex] = "a0";
                 }
-                if (correctTape(tape)) {
+                if (isCorrectTape(tape)) {
                     this.tape = tape;
                 }
             }
         }
+
     }
 
-    public TuringMachine (String[] tape , LinkedList<String> alphabet , int currentIndex) {
-        if (currentIndex >= -1 && currentIndex < tape.length && alphabet != null) {
+    public TuringMachine(String[] tape, LinkedList<String> alphabet, LinkedList<Condition> conditionsMT, int currentIndex, Condition currentCondition, Condition stopCondition) {
+        if (currentIndex < tape.length && alphabet != null && conditionsMT != null && currentCondition != null && stopCondition != null) {
             this.alphabet = alphabet;
             alphabet.add("a0");
             fillUpTape(tape);
+            TuringMachine.conditionsMT = conditionsMT;
             this.currentIndex = currentIndex;
+            this.currentCondition = currentCondition;
+            this.stopCondition = stopCondition;
         }
     }
 
-    public void writeCommand (String value , String motion) {
-        tape[currentIndex] = value;
-        if(motion.equals("L")){
+    public static class Condition {
+        private String defineValue;
+        private Condition nextCondition;
+        private String value;
+        private String motion;
+
+
+        public Condition(String defineValue, String value, String motion, Condition nextCondition) {
+
+            if (defineValue != null && nextCondition != null && value != null && motion != null) {
+                this.defineValue = defineValue;
+                this.nextCondition = nextCondition;
+                this.value = value;
+                this.motion = motion;
+            }
+        }
+
+        public Condition() {
+            // Stop - condition;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Condition condition = (Condition) o;
+            return Objects.equals(defineValue, condition.defineValue);
+
+        }
+    }
+
+    public void runMachine () {
+        displayTape();
+        while (currentCondition != stopCondition) {
+            while (currentCondition.defineValue.equals(tape[currentIndex])) {
+                executeCommand(currentCondition);
+                if (currentCondition.nextCondition == stopCondition) {
+                    break;
+                }
+            }
+            currentCondition = currentCondition.nextCondition;
+        }
+        displayTape();
+    }
+
+    public void executeCommand (Condition condition) {
+        tape[currentIndex] = condition.value;
+        if(condition.motion.equals("L")){
             if (hasPrevious()) {
                 -- currentIndex;
             }
         }
-        if(motion.equals("R")){
+        if(condition.motion.equals("R")){
             if (hasNext()) {
                 ++ currentIndex;
             }
         }
     }
 
-    public boolean hasNext () {
+    private boolean hasNext () {
         return currentIndex + 1 < tape.length;
     }
 
-    public boolean hasPrevious () {
-        return currentIndex - 1 >= -1;
+    private boolean hasPrevious () {
+        return currentIndex - 1 >= 0;
+    }
+
+    public void displayCurrentIndex () {
+        System.out.println(currentIndex);
     }
 
 }
